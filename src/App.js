b82566e3ae385cae2c2fa4ee2556c13c0e11f704
JULNAVAR-T490\julnavar <julieta.navarro@oracle.com>
2021-04-29 14:47:18 -0600

    enable bg with particles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
 
+import Particles from 'react-particles-js';
+import particlesConfig from './config/configParticles';
+
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <p>soon</p>
+    <div className="App" style={{ position: 'relative', overflow: "hidden" }}>
+      <div style={{ position: 'absolute'}}>
+        <Particles height="100vh" width="100vw" params={particlesConfig} />
+      </div>
+      <header className="App-header">
+        <p>
+          soon
+        </p>
       </header>
     </div>
   );
